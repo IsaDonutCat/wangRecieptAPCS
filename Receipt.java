6b256b5322efb651e36590d493873e436604d861
Isabella Wang <wangisa26@7hills.org>
2023-08-30 13:11:20 -0400

    Twyford

--- Receipt.java
+++ Receipt.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class Receipt 
 {
 
     public static void main (String[] args)
-    {
+    { 
         System.out.println("Hello, World");
+        System.out.println("Hello, me");
     }
 
 }
