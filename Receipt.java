3b946ad50f49ab87aeaf3ff950bb44a6845c4d6a
Isabella Wang <wangisa26@7hills.org>
2023-09-01 12:18:21 -0400

    fixed bugs

--- Receipt.java
+++ Receipt.java
@@ -10,22 +10,22 @@ public class Receipt
         System.out.println("Welcome to my store! I sell breakfast items.");
         System.out.println("Please type the quantity that you want of an item"); 
         System.out.print("Coffee:");
-        double coffee = myObject.nextInt() * 2.99;
+        double coffee = input.nextInt() * 2.99;
 
         System.out.print("Bacon: ");
-        double bacon = myObject.nextInt() * 0.99;
+        double bacon = input.nextInt() * 0.99;
 
         System.out.print("Waffles: ");
-        double waffles = myObject.nextInt() * 2.99;
+        double waffles = input.nextInt() * 2.99;
 
         System.out.print("Cereal: ");
-        double waffles = myObject.nextInt() * 1.99;
+        double waffles = input.nextInt() * 1.99;
 
         System.out.print(s:"Fruit:");
-        double waffles = myObject.nextInt() * 1.99;
+        double waffles = input.nextInt() * 1.99;
 
         System.out.print("Omelets:");
-        double eggs = myObject.nextInt() * 1.99;
+        double eggs = input.nextInt() * 1.99;
 
         System.out.println("*");
         
