9db762dd18cbefea90980062086e47a74d206abc
Isabella Wang <wangisa26@7hills.org>
2023-09-01 12:16:56 -0400

    added items to buy + calculated price

--- Receipt.java
+++ Receipt.java
@@ -5,8 +5,30 @@ public class Receipt
 
     public static void main (String[] args)
     { 
-        System.out.println("Hello, World");
-        System.out.println("Stan Mizuki Akiyama");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Welcome to my store! I sell breakfast items.");
+        System.out.println("Please type the quantity that you want of an item"); 
+        System.out.print("Coffee:");
+        double coffee = myObject.nextInt() * 2.99;
+
+        System.out.print("Bacon: ");
+        double bacon = myObject.nextInt() * 0.99;
+
+        System.out.print("Waffles: ");
+        double waffles = myObject.nextInt() * 2.99;
+
+        System.out.print("Cereal: ");
+        double waffles = myObject.nextInt() * 1.99;
+
+        System.out.print(s:"Fruit:");
+        double waffles = myObject.nextInt() * 1.99;
+
+        System.out.print("Omelets:");
+        double eggs = myObject.nextInt() * 1.99;
+
+        System.out.println("*");
+        
     }
 
 }
