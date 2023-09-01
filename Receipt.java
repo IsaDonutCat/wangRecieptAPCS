296114b39b0a3571dcb5013a7a9981275e750f4a
Isabella Wang <wangisa26@7hills.org>
2023-09-01 12:06:13 -0400

    modified method

--- Receipt.java
+++ Receipt.java
@@ -6,7 +6,7 @@ public class Receipt
     public static void main (String[] args)
     { 
         System.out.println("Hello, World");
-        System.out.println("Hello, me");
+        System.out.println("Stan Mizuki Akiyama");
     }
 
 }
