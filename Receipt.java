77d8ffb03921eaa8cea2b331d2a2e44d9e646db5
Isabella Wang <wangisa26@7hills.org>
2023-09-06 13:02:11 -0400

    fixing it so more than single digit fixing alignment

--- Receipt.java
+++ Receipt.java
@@ -11,27 +11,29 @@ public class Receipt
         System.out.println("Please type the quantity that you want of an item"); 
         System.out.println();
 
-        System.out.print("Coffee (2.99 per cup):");
+        System.out.print("Coffee (2.99 per cup) - MAX 9:");
         double cof = input.nextInt() * 2.99;
-        String coffee = String.valueOf(cof);
+        int COF = (int) cof;
+        total = String.valueOf(COF);
 
-        System.out.print("Bacon (0.99 per bacon slice): ");
-        double bac = input.nextInt() * 0.99;
+        System.out.print("Bacon (0.99 per bacon slice - MAX 9): ");
+        double bac = input.nextInt() * 0.99; 
+        int COF = (int) cof;
         String bacon = String.valueOf(bac);
 
-        System.out.print("Waffles (3.99 for two): ");
+        System.out.print("Waffles (3.99 for two - MAX 9): ");
         double waf = input.nextInt() * 3.99;
         String waffles = String.valueOf(waf);
 
-        System.out.print("Cereal (1.99 per cup): ");
+        System.out.print("Cereal (1.99 per cup - MAX 9): ");
         double cer = input.nextInt() * 1.99;
         String cereal = String.valueOf(cer);
 
-        System.out.print("Fruit (1.99 per cup):");
+        System.out.print("Fruit (1.99 per cup - MAX 9):");
         double fru = input.nextInt() * 1.99;
         String fruit = String.valueOf(fru);
 
-        System.out.print("Omelets (1.99 per two eggs):");
+        System.out.print("Omelets (1.99 per two eggs - MAX 9):");
         double eggy = input.nextInt() * 1.99;
         String eggs = String.valueOf(eggy);
 
@@ -102,21 +104,22 @@ public class Receipt
         System.out.println("****************************************");
 
         double tot = eggy + fru + cof + bac + waf + cer;
-        String total = String.valueOf(tot);
+        int inTot = (int) tot;
+        String total = String.valueOf(inTot); 
 
         System.out.print("SUBTOTAL:");
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i < (28 - total.length()); i++)
         {
             System.out.print(" ");
         }
         System.out.println(String.format("%.2f", tot));
         
         tot *= 1.025;
-        total = String.valueOf(tot);
-        total = total.substring(0, 4);
+        inTot = (int) tot;
+        total = String.valueOf(inTot); // length of numbers before the decimal. d
 
         System.out.print("TOTAL:");
-        for (int i = 0; i < (33 - total.length()); i++)
+        for (int i = 0; i < (31 - total.length()); i++)
         {
             System.out.print(" ");
         }
